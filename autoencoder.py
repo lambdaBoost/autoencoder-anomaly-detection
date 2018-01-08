4f276e9158fdd50ea9654affb5f133f4a1df4c36
Alex Hall <northern.bantam@gmail.com>
2018-01-08 18:10:36 +0000

    added model

--- autoencoder.py
+++ autoencoder.py
@@ -25,13 +25,13 @@ from keras import regularizers
 from sklearn.preprocessing import StandardScaler
 
 #import dataset
-df=pd.read_csv("C:\\Users\\alex.hall\\Documents\\datasets\\spy-plane-finder\\planes_features.csv")
+df=pd.read_csv("C:\\Users\\Alex\\Documents\\datasets\\spy-plane-finder\\planes_features.csv")
 
 #convert type column to integer (there are probably better approaches to use here)
 df['type']=df['type'].astype('category').cat.codes
 
 #import labelled aircraft
-test_ident = pd.read_csv("C:\\Users\\alex.hall\\Documents\\datasets\\spy-plane-finder\\train.csv")
+test_ident = pd.read_csv("C:\\Users\\Alex\\Documents\\datasets\\spy-plane-finder\\train.csv")
 
 #get a 'testing set' and leave the rest of the data for actual analysis (note the different context of testing to normal for this model)
 test_set=df[df['adshex'].isin(test_ident['adshex'])]
@@ -41,8 +41,40 @@ df=df[~df['adshex'].isin(test_ident['adshex'])]
 unlabelled_test=test_set.drop(['adshex']) #drop labels
 df=df.drop(['adshex'])
 
-#apply model to test set and evaluate by creating error sf from the predictions. Take the top 60 or so errors and see how they match up with the removed adshexes (join with train data)
+#apply model to test set and evaluate by creating error sf from the predictions. 
+#Take the top 60 or so errors and see how they match up with the removed adshexes (join with train data)
 
+#define layers
+input_dim = unlabelled_test.shape[1]
+encoding_dim = int(input_dim/2)
+
+input_layer = Input(shape=(input_dim, ))
+encoder = Dense(encoding_dim, activation="tanh", 
+                activity_regularizer=regularizers.l1(10e-5))(input_layer)
+encoder = Dense(int(encoding_dim / 2), activation="relu")(encoder)
+decoder = Dense(int(encoding_dim / 2), activation='tanh')(encoder)
+decoder = Dense(input_dim, activation='relu')(decoder)
+autoencoder = Model(inputs=input_layer, outputs=decoder)
+
+
+nb_epoch = 100
+batch_size = 32
+autoencoder.compile(optimizer='adam', 
+                    loss='mean_squared_error', 
+                    metrics=['accuracy'])
+checkpointer = ModelCheckpoint(filepath="model.h5",
+                               verbose=0,
+                               save_best_only=True)
+tensorboard = TensorBoard(log_dir='./logs',
+                          histogram_freq=0,
+                          write_graph=True,
+                          write_images=True)
+history = autoencoder.fit(unlabelled_test, unlabelled_test,
+                    epochs=nb_epoch,
+                    batch_size=batch_size,
+                    shuffle=True,
+                    verbose=1,
+                    callbacks=[checkpointer, tensorboard]).history
 #apply model to df and see what we get
 
 
