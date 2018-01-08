3b4e2e9c513d6bd3f57e3b89cdd9ddce2e55fd37
Alex Hall <northern.bantam@gmail.com>
2018-01-08 22:37:52 +0000

    Autoencoder added

--- autoencoder.py
+++ autoencoder.py
@@ -33,19 +33,29 @@ df['type']=df['type'].astype('category').cat.codes
 #import labelled aircraft
 test_ident = pd.read_csv("C:\\Users\\Alex\\Documents\\datasets\\spy-plane-finder\\train.csv")
 
-#get a 'testing set' and leave the rest of the data for actual analysis (note the different context of testing to normal for this model)
-test_set=df[df['adshex'].isin(test_ident['adshex'])]
+#use the labelled data as a train/test set (note the different context of testing to normal for this model)
+labelled_data=df[df['adshex'].isin(test_ident['adshex'])]
+labelled_data=pd.merge(labelled_data,test_ident,on=['adshex','adshex'])
+labelled_data['type']=labelled_data['type'].astype('category').cat.codes
+labelled_data=labelled_data.drop(['adshex'],axis=1)
+
 df=df[~df['adshex'].isin(test_ident['adshex'])]
+df=df.drop(['adshex'],axis=1)
 
 #preprocess - consider scaling/standardising at this point
-unlabelled_test=test_set.drop(['adshex']) #drop labels
-df=df.drop(['adshex'])
+train_set, test_set = train_test_split(labelled_data, test_size=0.2,random_state=57)
+train_set = train_set[train_set['class'] == 'other']
+train_set = train_set.drop(['class'], axis=1)
+test_set = test_set.drop(['class'], axis=1)
+
+train_set = train_set.values
+test_set = test_set.values
 
 #apply model to test set and evaluate by creating error sf from the predictions. 
 #Take the top 60 or so errors and see how they match up with the removed adshexes (join with train data)
 
 #define layers
-input_dim = unlabelled_test.shape[1]
+input_dim = test_set.shape[1]
 encoding_dim = int(input_dim/2)
 
 input_layer = Input(shape=(input_dim, ))
@@ -57,7 +67,7 @@ decoder = Dense(input_dim, activation='relu')(decoder)
 autoencoder = Model(inputs=input_layer, outputs=decoder)
 
 
-nb_epoch = 100
+nb_epoch = 25
 batch_size = 32
 autoencoder.compile(optimizer='adam', 
                     loss='mean_squared_error', 
@@ -69,13 +79,18 @@ tensorboard = TensorBoard(log_dir='./logs',
                           histogram_freq=0,
                           write_graph=True,
                           write_images=True)
-history = autoencoder.fit(unlabelled_test, unlabelled_test,
+history = autoencoder.fit(train_set,train_set,
                     epochs=nb_epoch,
                     batch_size=batch_size,
                     shuffle=True,
+                    validation_data=(test_set,test_set),
                     verbose=1,
                     callbacks=[checkpointer, tensorboard]).history
-#apply model to df and see what we get
+
+autoencoder=load_model('model.h5')
+                          
+#predict on testing set
+predictions=autoencoder.predict(test_set)
 
 
 
